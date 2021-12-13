3f7f3f01fe6063b32c0351fbc4b0e45f8ef2fa00
Pegasidev <santinosuntaxi@gmail.com>
2021-12-13 00:07:48 -0500

    final correction for windows running

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -3,21 +3,50 @@ class apache {
     command => '/usr/bin/apt-get update'
   }
   
-   exec { 'php-dependencies':
-    command => '/usr/bin/apt-get install -y ghostscript \
-                 libapache2-mod-php \
-                 php \
-                 php-bcmath \
-                 php-curl \
-                 php-imagick \
-                 php-intl \
-                 php-json \
-                 php-mbstring \
-                 php-mysql \
-                 php-xml \
-                 php-zip 
-    '
+ exec { 'php-dependencies1':
+    command => '/usr/bin/apt-get install -y ghostscript'
   }
+  
+   exec { 'php-dependencies2':
+    command => '/usr/bin/apt-get install -y libapache2-mod-php'
+  }
+
+ exec { 'php-dependencies3':
+    command => '/usr/bin/apt-get install -y php'
+  }
+
+ exec { 'php-dependencies4':
+    command => '/usr/bin/apt-get install -y php-curl'
+  }
+
+ exec { 'php-dependencies5':
+    command => '/usr/bin/apt-get install -y php-imagick'
+  }
+
+  exec { 'php-dependencies6':
+    command => '/usr/bin/apt-get install -y  php-intl'
+  }
+
+  exec { 'php-dependencies7':
+    command => '/usr/bin/apt-get install -y php-json'
+  }
+
+  exec { 'php-dependencies8':
+    command => '/usr/bin/apt-get install -y php-mbstring'
+  }
+
+   exec { 'php-dependencies9':
+    command => '/usr/bin/apt-get install -y  php-mysql'
+  }
+
+   exec { 'php-dependencies10':
+    command => '/usr/bin/apt-get install -y php-xml'
+  }
+
+   exec { 'php-dependencies11':
+    command => '/usr/bin/apt-get install -y php-zip'
+  }
+
 
   package { 'apache2':
     ensure => installed,
